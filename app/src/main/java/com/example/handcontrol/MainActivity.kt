53b94cf5e5a51d5054ed70b29a1e85c1f5351b28
Aleksandr Prokopyev <aleksandr.prokopyev@jetbrains.com>
2020-11-02 00:23:47 +0300

    Add Russian language and refactor

--- app/src/main/java/com/example/handcontrol/MainActivity.kt
+++ app/src/main/java/com/example/handcontrol/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.handcontrol
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -8,86 +9,76 @@ import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.example.handcontrol.R
 import java.util.*
 
 
-class MainActivity : AppCompatActivity(), RecognitionListener {
+class MainActivity : AppCompatActivity() {
     private val permission = 100
     private lateinit var returnedText: TextView
-    private lateinit var toggleButton: ToggleButton
+    private lateinit var recordButton: ImageView
     private lateinit var progressBar: ProgressBar
     private lateinit var speech: SpeechRecognizer
     private lateinit var recognizerIntent: Intent
+    private lateinit var langSelectRadioGroup: RadioGroup
     private var logTag = "VoiceRecognitionActivity"
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-
-
-
         setContentView(R.layout.activity_main)
         title = "VoiceRecognition"
         returnedText = findViewById(R.id.textView)
         progressBar = findViewById(R.id.progressBar)
-        toggleButton = findViewById(R.id.toggleButton)
-        progressBar.visibility = View.VISIBLE
-        speech = SpeechRecognizer.createSpeechRecognizer(this)
-        returnedText.text = "HELLOPAGE"
-        Log.i(logTag, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this))
-//        speech.setRecognitionListener(this)
-        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "US-en")
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        recordButton = findViewById(R.id.imageView)
+        langSelectRadioGroup = findViewById(R.id.radioLang)
 
+        progressBar.visibility = View.VISIBLE
+        progressBar.isIndeterminate = false
+        progressBar.max = 100
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            progressBar.min = 0
+        }
+        speech = SpeechRecognizer.createSpeechRecognizer(this)
+        returnedText.text = ""
+        Log.i(logTag, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this))
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "RU-ru")
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "RU-ru")
+        langSelectRadioGroup.setOnCheckedChangeListener { group, id ->
+            recognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE, when (findViewById<RadioButton>(id).text) {
+                    "Русский" -> "RU-ru"
+                    else -> "US-en"
+                }
+            )
+        }
         recognizerIntent.putExtra(
             RecognizerIntent.EXTRA_LANGUAGE_MODEL,
             RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
         )
 
-
-
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
-        toggleButton.setOnCheckedChangeListener { _, isChecked ->
-
-
-//            if (SpeechRecognizer.isRecognitionAvailable(getApplicationContext())) {
-                if (isChecked) {
-                    progressBar.visibility = View.VISIBLE
-                    progressBar.isIndeterminate = true
-                    ActivityCompat.requestPermissions(this@MainActivity,
+        recordButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
                         arrayOf(Manifest.permission.RECORD_AUDIO),
-                        permission)
-                } else {
-                    progressBar.isIndeterminate = false
-                    progressBar.visibility = View.VISIBLE
+                        permission
+                    )
+                }
+                MotionEvent.ACTION_UP -> {
                     speech.stopListening()
                 }
-//            }
+            }
+            true
         }
     }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//         when (requestCode) {
-//            REQ_CODE_SPEECH_INPUT -> {
-//                if (resultCode == RESULT_OK && null != data) {
-//                    val result = data
-//                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-//                    txtSpeechInput.setText(result!![0])
-//                }
-//            }
-//        }
-//    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String?>,
@@ -98,6 +89,7 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
             permission -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager
                     .PERMISSION_GRANTED
             ) {
+                speech.setRecognitionListener(getListener())
                 speech.startListening(recognizerIntent)
             } else {
                 Toast.makeText(
@@ -107,44 +99,15 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
             }
         }
     }
+
     override fun onStop() {
         super.onStop()
         speech.destroy()
         Log.i(logTag, "destroy")
     }
-    override fun onReadyForSpeech(params: Bundle?) {
-        TODO("Not yet implemented")
-    }
-    override fun onRmsChanged(rmsdB: Float) {
-        progressBar.progress = rmsdB.toInt()
-    }
-    override fun onBufferReceived(buffer: ByteArray?) {
-        TODO("Not yet implemented")
-    }
-    override fun onPartialResults(partialResults: Bundle?) {
-        TODO("Not yet implemented")
-    }
-    override fun onEvent(eventType: Int, params: Bundle?) {
-        TODO("Not yet implemented")
-    }
-    override fun onBeginningOfSpeech() {
-        Log.i(logTag, "onBeginningOfSpeech")
-        progressBar.isIndeterminate = false
-        progressBar.max = 10
-    }
-    override fun onEndOfSpeech() {
-        progressBar.isIndeterminate = true
-        toggleButton.isChecked = false
-    }
-    override fun onError(error: Int) {
-        val errorMessage: String = getErrorText(error)
-        Log.d(logTag, "FAILED $errorMessage")
-        returnedText.text = errorMessage
-        toggleButton.isChecked = false
-    }
+
     private fun getErrorText(error: Int): String {
-        var message = ""
-        message = when (error) {
+        return when (error) {
             SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
             SpeechRecognizer.ERROR_CLIENT -> "Client side error"
             SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
@@ -156,17 +119,51 @@ class MainActivity : AppCompatActivity(), RecognitionListener {
             SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
             else -> "Didn't understand, please try again."
         }
-        return message
     }
-    override fun onResults(results: Bundle?) {
-        Log.i(logTag, "onResults")
-        val matches = results!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-        var text = ""
-        if (matches != null) {
-            for (result in matches) text = """
-          $result
-          """.trimIndent()
+
+    private fun getListener() = object : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle?) {
+            Unit //"Not yet implemented")
         }
-        returnedText.text = text
+
+        override fun onBeginningOfSpeech() {
+            Unit //"Not yet implemented")
+        }
+
+        override fun onRmsChanged(rmsdB: Float) {
+            // if rmsDb is within -10 .. 10, then move it to positive numbers and stretch to 100
+            progressBar.progress = ((rmsdB + 10) / 20 * 100).toInt()
+        }
+
+        override fun onBufferReceived(buffer: ByteArray?) {
+            Unit //"Not yet implemented")
+        }
+
+        override fun onEndOfSpeech() {
+            Unit //"Not yet implemented")
+        }
+
+        override fun onError(error: Int) {
+            if (error == SpeechRecognizer.ERROR_NO_MATCH || error == SpeechRecognizer.ERROR_CLIENT)
+                return // just ignore
+            val errorMessage: String = getErrorText(error)
+            Log.d(logTag, "FAILED $errorMessage")
+            returnedText.text = errorMessage
+        }
+
+        override fun onResults(results: Bundle?) {
+            results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.firstOrNull()?.let {
+                returnedText.text = it
+            }
+        }
+
+        override fun onPartialResults(partialResults: Bundle?) {
+            Unit //"Not yet implemented")
+        }
+
+        override fun onEvent(eventType: Int, params: Bundle?) {
+            Unit //"Not yet implemented")
+        }
+
     }
 }
